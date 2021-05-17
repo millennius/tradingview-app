aecb040dcdfc06f39f187c6ad89fea68a327da52
Ajay Srinivas <ajaysrinivas530@gmail.com>
2021-05-17 12:27:42 +0530

    Final commit for v0.1.0

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # tradingview-app
+
 An electron based desktop client application for tradingview.com
+
+Clone this repository if you want to build from source.
+Run
+
+```
+$ npm install
+$ npm run generate-release
+$ npm run package-windows
+```
+
+If you face $Product.versionNumber not found error, then go to package.json inside the folder generated after `generate-release` script (it is usually "TradingView-win32-x64" or similar) and then rename the package name to contain no dash or hyphens.
+
+Run `package-windows` script again.
--- packager.js
+++ packager.js
@@ -7,10 +7,10 @@ resultPromise = electronInstaller.createWindowsInstaller({
   exe: "TradingView.exe", //Find the name of the exe in appDirectory
   noMsi: false, //No need for mis! [write picture description here] (https://img-blog.csdn.net/20180712225817503?watermark/2/text/aHR0cHM6Ly9ibG9nLmNzZG4ubmV0L3FxXzI2NjI2MTEz/font/5a6L5L2T/fontsize/400/fill/I0JBQkdisk /70)
   setupIcon: "./TradingView-win32-x64/resources/app/icon.ico", //The icon file address of the generated exe file
-  setupExe: "tradingview-setup-win64-0.0.1.exe",
-  version: "0.0.1",
+  setupExe: "tradingview-setup-win64-0.1.0.exe",
+  version: "0.1.0",
   title: "TradingView",
-  setupMsi: "tradingview-setup-win64-0.0.1.exe",
+  // setupMsi: "tradingview-setup-win64.exe",
 });
 
 resultPromise.then(
